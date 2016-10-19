c8282930d72245c979991dc862a4852dcde41175
Liubomyr.Svyshchuk <liubomyr.svyshchuk@gmail.com>
2016-10-19 10:00:09 +0300

    Revert "s3fs CA on Centos 7 / version 1.80 updates"

--- manifests/dependencies.pp
+++
@@ -1,8 +0,0 @@
-class s3fs::dependencies {
-
-  # Making sure fuse and s3fs are not installed from package manager
-  package { $::s3fs::conflict_pkgs : ensure => absent, }
-
-  package { $::s3fs::dependency_pkgs : ensure => installed, }
-
-}
