15ab662ff2a53df4504a45ac466316f12b3dd0de
chris.jomaron <chris.jomaron@moneysupermarket.com>
2017-04-26 11:43:36 +0100

    pathname is wrong

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,26 +14,26 @@ class s3fs::install {
   exec {'extract-s3fs':
     cwd     => $s3fs::tarball_dir,
     command => "tar zxf ${s3fs_tarball}",
-    creates => "${s3fs::tarball_dir}/s3fs-fuse-${s3fs::s3fs_version}",
+    creates => "${s3fs::tarball_dir}/s3fs-${s3fs::s3fs_version}",
     require => Wget::Fetch['s3fs'],
   }
   exec {'autogen-configure-s3fs':
-    cwd      => "${s3fs::tarball_dir}/s3fs-fuse-${s3fs::s3fs_version}/",
+    cwd      => "${s3fs::tarball_dir}/s3fs-${s3fs::s3fs_version}/",
     provider => 'shell',
     command  => './autogen.sh',
-    creates  => "${s3fs::tarball_dir}/s3fs-fuse-${s3fs::s3fs_version}/configure",
+    creates  => "${s3fs::tarball_dir}/s3fs-${s3fs::s3fs_version}/configure",
     require  => [ Exec['extract-s3fs'], Exec['compile-fuse'], ],
   }
   exec {'configure-s3fs':
-    cwd         => "${s3fs::tarball_dir}/s3fs-fuse-${s3fs::s3fs_version}/",
+    cwd         => "${s3fs::tarball_dir}/s3fs-${s3fs::s3fs_version}/",
     provider    => 'shell',
     environment => 'PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/lib64/pkgconfig/',
     command     => './configure --prefix=/usr',
-    creates     => "${s3fs::tarball_dir}/s3fs-fuse-${s3fs::s3fs_version}/Makefile",
+    creates     => "${s3fs::tarball_dir}/s3fs-${s3fs::s3fs_version}/Makefile",
     require     => [ Exec['extract-s3fs'], Exec['compile-fuse'], Exec['autogen-configure-s3fs'], ],
   }
   exec {'compile-s3fs':
-    cwd      => "${s3fs::tarball_dir}/s3fs-fuse-${s3fs::s3fs_version}/",
+    cwd      => "${s3fs::tarball_dir}/s3fs-${s3fs::s3fs_version}/",
     provider => 'shell',
     command  => 'make && make install',
     unless   => "/usr/bin/s3fs --version | grep ${s3fs::s3fs_version}",
